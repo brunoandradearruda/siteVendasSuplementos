486c6f49c9f3512cd4ca39160e3fdd7a4271d9e8
“bruno <brunoandradearruda@gmail.com>
2025-11-26 14:38:07 -0300

    Finalizado

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,13 @@ const nextConfig = {
   output: 'standalone',
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'http2.mlstatic.com',
-        port: '',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'images.tcdn.com.br' },
+      { protocol: 'https', hostname: 'integralmedica.vtexassets.com' },
+      { protocol: 'https', hostname: 'http2.mlstatic.com' },
+      { protocol: 'https', hostname: 'darklab.ind.br' },
+      { protocol: 'https', hostname: 'probiotica.vtexassets.com' },
+      // ADICIONADO: Autorização para imagens do blog (Unsplash)
+      { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
   },
 };
